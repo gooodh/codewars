48d9f292eeb9dd46c1e4bace4c83dc1bce2145cf
nikuliny <s.b.nikuliny@gmail.com>
2022-03-21 19:35:46 +0700

    codewars (example4) sort lambda
    тупой тест не проходит '2000 11 11 10003 22 123 1234000 44444444 9999' should equal '11 11 2000 10003 22 123 1234000 44444444 9999'

--- example4.py
+++ example4.py
@@ -1,34 +1,35 @@
-def find_it(n):
-    list_w = n.split(' ')
+def order_weight(strng):
+    list_w = strng.split(' ')
+
     dict1 = {}
+    dict2 = {}
+    n = 0
 
     for i in list_w:
+        n += 1
         int_w = int(i)
-        int_ww = int_w
+        dict1[n] = int_w
         tot = 0
 
-        while int_w > 0:
+        while int_w > 0:  # Программа нахождения суммы всех цифр данного числа
             dig = int_w % 10
             tot += dig
             int_w = int_w // 10
 
-        dict1[int_ww] = tot
+        dict2[n] = tot
+
+    sorted_dict = sorted(dict2.items(), key=lambda x: x[1])  # Sort
+    dict2 = dict(sorted_dict)
+    mass = ''
+    for i in dict2.keys():
+        for k in dict1.keys():
+            if k == i:
+                mass += str(dict1[i]) + " "
+                break
 
 
-        sorted_values = sorted(dict1.values())  # Sort the values
-        sorted_dict = {}
-        for i in sorted_values:
-            for k in dict1.keys():
-                if dict1[k] == i:
-                    sorted_dict[k] = dict1[k]
-                    break
-
-
-        print(sorted_dict)
-
-
-
+    print(mass.strip())
 
 
 if __name__ == '__main__':
-    find_it('103 123 4444 99 2000')
+    order_weight('2000 10003 1234000 44444444 9999 11 11 22 123')
